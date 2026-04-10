d1ff1360ecef382a5d47bf1c7a7ae35d5340884e
yekta <ngyekta@gmail.com>
2026-04-11 02:24:30 +0300

    Sign in form loading state

--- components/sign-in-form.tsx
+++ components/sign-in-form.tsx
@@ -4,6 +4,7 @@ import { useAuth } from "@/components/auth-provider";
 import Logo from "@/components/icons/logo";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { LoaderIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -13,7 +14,7 @@ type TProps = {
 
 export default function SignInForm({ className }: TProps) {
   const [isSigningIn, setIsSigningIn] = useState(false);
-  const { signInWithGoogle, user } = useAuth();
+  const { signInWithGoogle, user, loading } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
@@ -36,22 +37,29 @@ export default function SignInForm({ className }: TProps) {
         className,
       )}
     >
-      <div className="max-w-md text-center space-y-6">
-        <Logo className="mx-auto size-16" />
-        <h1 className="text-4xl font-bold tracking-tight text-foreground">
-          DeckNinja
-        </h1>
-        <p className="text-lg text-muted-foreground">
-          Master any subject with spaced repetition. Sign in to start learning.
-        </p>
-        <Button
-          onClick={handleSignIn}
-          className="w-full max-w-64"
-          isPending={isSigningIn}
-        >
-          Sign in with Google
-        </Button>
-      </div>
+      {loading ? (
+        <div className="w-full flex flex-col items-center justify-center">
+          <LoaderIcon className="size-6 animate-spin text-muted-foreground" />
+        </div>
+      ) : (
+        <div className="max-w-md text-center space-y-6">
+          <Logo className="mx-auto size-16" />
+          <h1 className="text-4xl font-bold tracking-tight text-foreground">
+            DeckNinja
+          </h1>
+          <p className="text-lg text-muted-foreground">
+            Master any subject with spaced repetition. Sign in to start
+            learning.
+          </p>
+          <Button
+            onClick={handleSignIn}
+            className="w-full max-w-64"
+            isPending={isSigningIn}
+          >
+            Sign in with Google
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
